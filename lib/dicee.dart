f74be2e2b70de193def9c8230310689444c27a67
Sophia-PeiJin-SU <tj790929@gmail.com>
2020-12-07 14:48:47 +1030

    ball

--- lib/dicee.dart
+++ lib/dicee.dart
@@ -59,3 +59,4 @@ class _DicePageState extends State<DicePage> {
   }
 }
 
+
---
+++ lib/magic8ball.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Magic8BallPage(),
+      ),
+    ),
+  );
+}
+
+class Magic8BallPage extends StatefulWidget {
+  @override
+  _Magic8BallPage createState() => _Magic8BallPage();
+}
+
+class _Magic8BallPage extends State<Magic8BallPage> {
+  int ballNumber = 1;
+
+  void randomBall(){
+    setState(() {
+      ballNumber = Random().nextInt(4) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                randomBall();
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
