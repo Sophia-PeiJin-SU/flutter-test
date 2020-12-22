791f1d6f094f0fc3eaff09007fa72f80be16ae02
denny61302 <denny61302@gmail.com>
2020-12-22 14:15:04 +1030

    weather project

---
+++ lib/weather/screens/loading_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../services/location.dart';
+import '../services/networking.dart';
+
+
+const apiKey = '807031fe9f3e3282667deb969fbdff57';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+
+
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
