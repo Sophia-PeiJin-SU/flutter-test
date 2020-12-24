8b101c7a3b00356132a4f8568b6b37cefc912c41
denny61302 <denny61302@gmail.com>
2020-12-24 16:33:54 +1030

    chat project

---
+++ lib/chat/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import './screens/welcome_screen.dart';
+import './screens/login_screen.dart';
+import './screens/registration_screen.dart';
+import './screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          headline6: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      }
+    );
+  }
+}
